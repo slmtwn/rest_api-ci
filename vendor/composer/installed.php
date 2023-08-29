f496b941f2ae0f96ff5b9323e459807f6e07e541
slmtwn <slmtwn@gmail.com>
2023-08-29 09:11:17 +0700

    edit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2044008cc40308ccb1e281f9fccfa824b1ec6790',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2044008cc40308ccb1e281f9fccfa824b1ec6790',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chriskacerguis/codeigniter-restserver' => array(
+            'pretty_version' => '3.1.5',
+            'version' => '3.1.5.0',
+            'reference' => '63c5ccad5581c3e0035c400aa929ba1ef0822418',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chriskacerguis/codeigniter-restserver',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
